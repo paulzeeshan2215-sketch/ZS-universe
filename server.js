8f28885c5c616fb885e7d59c2f3edb7e18fb7ff5
paulzeeshan2215-sketch <paulzeeshan2215@gmail.com>
2026-08-12 18:35:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = express();
 const fs = require("fs");
 const path = require("path");
 
-const app = express();
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.static(path.join(__dirname, 'public')));
 
